d9b0045c382dd677f2c8c75464fcacc3feb4582b
cyclopz66 <gustavomartinspinheiro@gmail.com>
2021-06-20 08:19:25 -0300

    add games route (get and post)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import pg from "pg";
-import { categoriesSchema } from "./schemas.js";
+import { categoriesSchema, gameSchema } from "./schemas.js";
 
 const { Pool } = pg;
 
@@ -43,6 +43,65 @@ app.post("/categories", async (req, res) => {
   }
 });
 
+app.get("/games", async (req, res) => {
+  const { name } = req.query;
+  console.log(`${name}%`, req.query);
+  if (name) {
+    const result = await connection.query(
+      `SELECT g.*, c.name as "categoryName" FROM (SELECT * FROM games WHERE games.name like $1) as g JOIN categories as c ON g."categoryId" = c.id;`,
+      [`${name.charAt(0).toUpperCase() + name.slice(1)}%`]
+    );
+    console.log(result.command);
+    res.status(200).send(result.rows);
+  } else {
+    const result = await connection.query(
+      'SELECT g.*, c.name as "categoryName" FROM games AS g JOIN categories AS c ON g."categoryId" = c.id'
+    );
+    res.status(200).send(result.rows);
+  }
+});
+
+app.post("/games", async (req, res) => {
+  let { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  try {
+    await gameSchema.validateAsync({
+      name,
+      image,
+      stockTotal,
+      categoryId,
+      pricePerDay,
+    });
+    const categoriesQuery = await connection.query(
+      "SELECT * FROM categories WHERE categories.id = $1",
+      [categoryId]
+    );
+    if (categoriesQuery.rows.length) {
+      const nameQuery = await connection.query(
+        "SELECT * FROM games WHERE name = $1",
+        [name]
+      );
+      if (nameQuery.rows.length) {
+        return res.sendStatus(409);
+      } else {
+        await connection.query(
+          'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',
+          [name, image, stockTotal, categoryId, pricePerDay]
+        );
+        return res.sendStatus(201);
+      }
+    } else {
+      console.log(categoriesQuery.rows);
+      return res.sendStatus(400);
+    }
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(400);
+  }
+});
+
 app.listen(4000, () => {
   console.log("Listening on port 4000");
 });
+
+console.log("Ba".charAt(0).toUpperCase() + "ba".slice(1));
--- src/schemas.js
+++ src/schemas.js
@@ -3,3 +3,11 @@ import Joi from "joi";
 export const categoriesSchema = Joi.object({
   name: Joi.string().required(),
 });
+
+export const gameSchema = Joi.object({
+  name: Joi.string().required(),
+  image: Joi.string().pattern(/^http(s|):\/\//),
+  stockTotal: Joi.number().min(1).required(),
+  categoryId: Joi.number().required(),
+  pricePerDay: Joi.number().min(1).required(),
+});
